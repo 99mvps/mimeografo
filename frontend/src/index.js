6304ef0f5d4f571348923cdd426fab31aae64459
aldous huxley <99mvps@gmail.com>
2023-08-13 21:40:14 -0300

    fix(ads): allow CORS to any requests

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,16 +8,7 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      callback(
-        null,
-        ['https://pagead2.googlesyndication.com'].includes(origin),
-      );
-    },
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
